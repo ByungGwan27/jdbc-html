ad740a5d3ff390fc1a3b37f7299c7dc9dc343437
byunggwan <byunggwan827@gamil.com>
2023-05-06 10:05:01 +0900

    5.06 초기 더 안볼듯.

--- HelloApp/src/main/java/com/yedam/member/domain/EventVO.java
+++ HelloApp/src/main/java/com/yedam/member/domain/EventVO.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
+//@AllArgsConstructor
 public class EventVO {
 	private String title;
 	private String startDate;
