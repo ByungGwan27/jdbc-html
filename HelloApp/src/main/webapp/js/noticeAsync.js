d16e2678a54d434d44dd748ec009a03cd62b46a4
user <user@DESKTOP-D8NAJEC>
2023-05-22 17:31:25 +0900

    5.22 수업 종료

--- HelloApp/src/main/webapp/js/noticeAsync.js
+++ HelloApp/src/main/webapp/js/noticeAsync.js
@@ -5,7 +5,18 @@
 
 async function loadData() {
 	let promise = await fetch('noticeListJson.do');
-	let promise1 = promise.json();
+	let promise1 = await promise.json(); // json -> object.
+	let fields = ['noticeId', 'noticeTitle', 'noticeWriter', 'attachFile']
+	promise1.forEach(function(item) {
+		console.log(item);
+		let tr = document.createElement('tr');
+		for(let prop of fields) {
+			let td = document.createElement('td');
+			td.innerText = item[prop];
+			tr.append(td);
+		}
+		document.getElementById('noticeList').append(tr);
+	});
 }
 
 document.addEventListener('DOMContentLoaded', function () {
